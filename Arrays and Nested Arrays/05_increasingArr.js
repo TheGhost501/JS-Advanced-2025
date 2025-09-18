b6e0b4052078dfc06de23e0c755fecc04f980377
TheGhost501 <ilianvnv@yahoo.com>
2025-09-18 21:41:11 +0100

    task 5 done

---
+++ Arrays and Nested Arrays/05_increasingArr.js
@@ -0,0 +1,24 @@
+function increase(inputArr) {
+    let newArr = [];
+    let currentMin = Number.NEGATIVE_INFINITY;
+
+    inputArr.forEach(element => { if(element >= currentMin) {
+        newArr.push(element);
+        currentMin = element;
+    }
+        
+    });
+    return newArr;
+}
+
+console.log(increase([1,
+    3,
+    8,
+    4,
+    10,
+    12,
+    3,
+    2,
+    24]
+))
+
