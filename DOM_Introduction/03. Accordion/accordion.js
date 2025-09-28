3e8e1644a78ee9fc9e6af71d2108e1b5c7c3e949
TheGhost501 <ilianvnv@yahoo.com>
2025-09-28 18:57:11 +0100

    task 3 done

--- DOM_Introduction/03. Accordion/accordion.js
+++ DOM_Introduction/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
-}
+    const button = document.getElementsByClassName("button")[0];
+    const extra = document.getElementById("extra");
+    
+    if (extra.style.display === "block") {
+        extra.style.display = "none";
+        button.textContent = "More";
+    } else {
+        extra.style.display = "block";
+        button.textContent = "Less";
+    }
+
+}  
