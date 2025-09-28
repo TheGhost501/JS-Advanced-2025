692e09b0c4b54e57533d0f870b68edca73d39c77
TheGhost501 <ilianvnv@yahoo.com>
2025-09-28 17:03:21 +0100

    task 2 done

--- DOM_Introduction/02. Pascal or Camel Case/solution.js
+++ DOM_Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById("text").value.toLowerCase().split(" ");
+  let type = document.getElementById("naming-convention").value;
+  let result = "";
+
+  if (type == "Camel Case") {
+    result = text
+      .map((word, index) =>
+        index === 0
+          ? word
+          : word.charAt(0).toUpperCase() + word.slice(1)
+      )
+      .join("");
+  } else if (type == "Pascal Case") {
+    result = text
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join("");
+  } else {
+    result = "Error!"
+  }
+
+  document.getElementById("result").textContent = result;
+
 }
