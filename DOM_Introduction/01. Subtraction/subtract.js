db07fcf3d7bcf6b0f0568bbf70baeab6387668f9
TheGhost501 <ilianvnv@yahoo.com>
2025-09-28 16:16:44 +0100

    task 1 DOM intro done

--- DOM_Introduction/01. Subtraction/subtract.js
+++ DOM_Introduction/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+
+    const num1 = Number(document.getElementById("firstNumber").value);
+    const num2 = Number(document.getElementById("secondNumber").value);
+    const result = num1 - num2;
+    document.getElementById("result").textContent = result;
+    console.log(result);
 }
