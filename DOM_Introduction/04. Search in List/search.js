0d79e48e7de9c25d24308b90c0134ce65b66a75e
TheGhost501 <ilianvnv@yahoo.com>
2025-10-02 18:15:49 +0100

    task 4,5 done

--- DOM_Introduction/04. Search in List/search.js
+++ DOM_Introduction/04. Search in List/search.js
@@ -1,3 +1,24 @@
 function search() {
-   // TODO
+   let list = document.querySelectorAll("#towns li");
+   let search = document.getElementById("searchText").value;
+   let result = document.getElementById("result");
+   let matches = 0;
+
+   list.forEach(li => {
+      li.style.fontWeight = "normal";
+      li.style.textDecoration = "none";
+   })
+
+
+   for (const town of list) {
+
+      if (town.textContent.includes(search)) {
+         town.style.fontWeight = "bold";
+         town.style.textDecoration = "underline";
+         matches++;
+      }
+
+   }
+   search = "";
+   result.textContent = `${matches} matches found`;
 }
--- DOM_Introduction/05. Table - Search Engine/solution.js
+++ DOM_Introduction/05. Table - Search Engine/solution.js
@@ -1,8 +1,26 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
+   const searchRef = document.getElementById("searchField");
+   const tableRowRef = document.querySelectorAll("tbody tr");
    function onClick() {
-      //   TODO:
+      const searchText = searchRef.value;
+      if (!searchText) {
+         return;
+      }
+      searchRef.value = "";
+      for (let i = 0; i < tableRowRef.length; i++) {
+         const tableDataRef = tableRowRef[i].querySelectorAll("td");
+         for (let col = 0; col < tableDataRef.length; col++) {
+            const tableData = tableDataRef[col].textContent;
+            if (tableData.includes(searchText)) {
+               tableRowRef[i].classList.add("select");
+               break;
+            } else {
+               tableRowRef[i].classList.remove("select");
 
+            }
+
+         }
+      }
    }
 }
