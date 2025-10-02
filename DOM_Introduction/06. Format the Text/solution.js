8b3c53797b060142b180816bd73274dbc50576ec
TheGhost501 <ilianvnv@yahoo.com>
2025-10-02 22:41:47 +0100

    task 6 done

--- DOM_Introduction/06. Format the Text/solution.js
+++ DOM_Introduction/06. Format the Text/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO
+  const input = document.getElementById("input").value;
+  const outputRef = document.getElementById("output");
+
+  const text = input.split(".").filter(x => !!x && x !== " ");
+
+  for (let i = 0; i < text.length; i += 3) {
+    let result = [];
+    for (let y = 0; y < 3; y++) {
+      if (!text[i + y]) {
+        break;
+      }
+      result.push(text[i + y])
+
+    }
+    let buff = result.join(".") + ".";
+    outputRef.innerHTML += `<p>${buff.trim()}</p>`;
+
+  }
 }
